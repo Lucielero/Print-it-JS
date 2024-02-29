bb7ecc800066f545a713166933eeaa4701f5068b
Lucie Leroux <lucieleroux93@gmail.com>
2024-02-29 10:26:16 +0100

    Element Selection and function

--- assets/script.js
+++ assets/script.js
@@ -18,16 +18,38 @@ const slides = [
 	}
 ]
 
+//Element selection
+const bannerImg = document.querySelector ('.banner-img') //main img
+const arrowLeft = document.querySelector ('.arrow_left')
+const arrowRight = document.querySelector ('.arrow_right')
+const dots = document.querySelector ('.dot') //all dots
+
+let currentIndex = 0 //index tracking
+
 //EventListener
 arrowRight.addEventListener ('click,' function() { //onclick event
-currentIndex = (currentIndex +1) //next img 
-slideDisplay (currentIndex, 'right') //new index and direction
-displayDot (currentIndex) //dot update
+	currentIndex = (currentIndex +1) //next img 
+	slideDisplay (currentIndex, 'right') //new index and direction
+	displayDot (currentIndex) //dot update
 })
 
 arrowLeft.addEventListener ('click,' function() {
-currentIndex=(currentIndex -1)
-slideDisplay (currentIndex, 'left')
-displayDot (currentIndex)
-})
+	currentIndex=(currentIndex -1)
+	slideDisplay (currentIndex, 'left')
+	displayDot (currentIndex)
+	})
+
+//Function 
+
+function slideDisplay (index, direction) {
+	if (currentIndex === 0 && direction ==='right') {
+		currentIndex = slides.length -1
+		}
+		else if (currentIndex ===slides.length -1 && direction ==='left') {
+			currentIndex = 0
+			}
+	const imgPath = 'assets/images/slideshow/slide1' //img path
+}
+	
+//Dots Function
 
